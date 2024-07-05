f0bcaa1ea73c29eed227cb8281ce8187c684b34e
Joshua Smart <josh@thesmarts.co.uk>
2024-07-05 22:26:18 +0100

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use axum::{routing::get, Router};
 use anyhow::Result;
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use tower_http::{services::ServeDir, trace::TraceLayer};
+use tracing::info;
 
 mod index;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .compact()
+        .init();
 
     let app = Router::new()
         .route("/", get(|| async { index::IndexTemplate }))
-        .nest_service("/assets", tower_http::services::ServeDir::new("assets"));
+        .nest_service("/assets", ServeDir::new("assets"))
+        .layer(TraceLayer::new_for_http());
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    let address = "0.0.0.0";
+    let port = 3000;
+
+    let listener = TcpListener::bind((address, port)).await?;
+    info!("Starting server on `{address}:{port}`");
     axum::serve(listener, app).await?;
 
     Ok(())
