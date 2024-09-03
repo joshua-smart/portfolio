18fe9b42e33c42c3f31e5da53a3f301e13ae15d6
Joshua Smart <josh@thesmarts.co.uk>
2024-09-03 17:38:59 +0100

    removed nixosModules from per-system deps

--- flake.nix
+++ flake.nix
@@ -75,69 +75,70 @@
           default = portfolio;
         };
 
-        nixosModules.default =
-          {
-            config,
-            lib,
-            pkgs,
-            ...
-          }:
-          let
-            inherit (lib)
-              mkEnableOption
-              mkIf
-              mkOption
-              types
-              concatStringsSep
-              ;
-            cfg = config.services.portfolio;
-            args = concatStringsSep " " [
-              "--port ${toString cfg.port}"
-              "--address ${cfg.address}"
-              "--asset-dir ${portfolio}/assets"
-              "--data-path \${STATE_DIRECTORY}/data.ron"
-            ];
-          in
-          {
-            options.services.portfolio = {
-              enable = mkEnableOption "portfolio";
-              port = mkOption {
-                type = types.port;
-                default = 3000;
-              };
-              address = mkOption {
-                type = types.str;
-                default = "0.0.0.0";
-              };
-              openFirewall = mkOption {
-                type = types.bool;
-                default = false;
-              };
-            };
-            config = mkIf cfg.enable {
-              networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [ cfg.port ];
-
-              systemd.services.portfolio = {
-                enable = true;
-
-                description = "Personal portfolio website.";
-                serviceConfig = {
-                  ExecStart = "${pkgs.portfolio}/bin/portfolio ${args}";
-                  StateDirectory = "portfolio";
-                };
-
-                wantedBy = [ "multi-user.target" ];
-                after = [ "network.target" ];
-                unitConfig = {
-                  StartLimitIntervalSec = 5;
-                  StartLimitBurst = 10;
-                };
-              };
-            };
-          };
-
         overlay = final: prev: { inherit portfolio; };
         overlays.default = self.overlay;
       }
-    );
+    )
+    // {
+      nixosModules.default =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          inherit (lib)
+            mkEnableOption
+            mkIf
+            mkOption
+            types
+            concatStringsSep
+            ;
+          cfg = config.services.portfolio;
+          args = concatStringsSep " " [
+            "--port ${toString cfg.port}"
+            "--address ${cfg.address}"
+            "--asset-dir ${pkgs.portfolio}/assets"
+            "--data-path \${STATE_DIRECTORY}/data.ron"
+          ];
+        in
+        {
+          options.services.portfolio = {
+            enable = mkEnableOption "portfolio";
+            port = mkOption {
+              type = types.port;
+              default = 3000;
+            };
+            address = mkOption {
+              type = types.str;
+              default = "0.0.0.0";
+            };
+            openFirewall = mkOption {
+              type = types.bool;
+              default = false;
+            };
+          };
+          config = mkIf cfg.enable {
+            networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [ cfg.port ];
+
+            systemd.services.portfolio = {
+              enable = true;
+
+              description = "Personal portfolio website.";
+              serviceConfig = {
+                ExecStart = "${pkgs.portfolio}/bin/portfolio ${args}";
+                StateDirectory = "portfolio";
+              };
+
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+              unitConfig = {
+                StartLimitIntervalSec = 5;
+                StartLimitBurst = 10;
+              };
+            };
+          };
+        };
+    };
 }
