055907c04f6ca834d32cf42d1d7d9ba285243940
Joshua Smart <josh@thesmarts.co.uk>
2025-11-02 18:21:04 +0000

    Added health check endpoint

--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -9,4 +9,9 @@ pub fn router() -> Router<AppState> {
     Router::new()
         .route("/", get(index::get))
         .route("/cv", get(cv::get))
+        .route("/health", get(health))
+}
+
+async fn health() -> String {
+    "healthy".to_string()
 }
